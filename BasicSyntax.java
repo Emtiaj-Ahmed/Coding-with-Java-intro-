b0a545327f289d4c873f1e23777553e5cf279282
Emtiaj Ahmed <emtiajahmed1@gmail.com>
2017-09-10 15:34:46 +0600

    Add files via upload

---
+++ BasicSyntax.java
@@ -0,0 +1,28 @@
+package beginner;
+
+public class BasicSyntax {
+	public static void main(String[] args) {
+		String name = "The Age of Emtiaj is ";
+		int i = 21;
+		System.out.println(name+i); //this function will add two variable 
+		int a=50, b=50;
+		//int b=50; 
+		//as like as C programming :P 
+		int c=a+b;
+		String message = "The addition of a and b is: " ;
+		System.out.println(message+c);
+		int d=a%b;
+		System.out.println(d);
+		//System.out.println(a%b);
+		
+		// Basic concepts are like as C programming. so dont need to worry about :D 
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
